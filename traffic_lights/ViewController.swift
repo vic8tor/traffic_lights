583a576f3e51cd2b416a6cf095b75a9b10530940
vic8tor <timongrinni@gmail.com>
2021-12-13 17:49:00 +0300

    Added a message

--- traffic_lights/ViewController.swift
+++ traffic_lights/ViewController.swift
@@ -6,14 +6,56 @@
 //
 
 import UIKit
-
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
-
-
+enum CurrentLight {
+    case red, yellow, green
 }
 
+class ViewController: UIViewController {
+    @IBOutlet var redView: UIView!
+    @IBOutlet var yellowView: UIView!
+    @IBOutlet var greenView: UIView!
+    @IBOutlet var switchButton: UIButton!
+    @IBOutlet weak var messageLabel: UILabel!
+    private let lithtIsOn: CGFloat = 1
+    private let lightIsOff = 0.4
+    private var currentLight = CurrentLight.red
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        switchButton.layer.cornerRadius = 10
+        redView.alpha = lightIsOff
+        yellowView.alpha = lightIsOff
+        greenView.alpha = lightIsOff
+}
+    override func viewWillLayoutSubviews() {
+        redView.layer.cornerRadius = redView.frame.size.width/2
+        yellowView.layer.cornerRadius = yellowView.frame.size.width/2
+        greenView.layer.cornerRadius = greenView.frame.size.width/2
+    }
+    
+    @IBAction func startSwitch() {
+        if switchButton.currentTitle == "Start" {
+        switchButton.setTitle("Next", for: .normal)
+        }
+        switch currentLight {
+        case .red:
+            greenView.alpha = lightIsOff
+            redView.alpha = lithtIsOn
+            currentLight = .yellow
+            messageLabel.textColor = .red
+            messageLabel.text = "STOP"
+        case .yellow:
+            redView.alpha = lightIsOff
+            yellowView.alpha = lithtIsOn
+            currentLight = .green
+            messageLabel.textColor = .yellow
+            messageLabel.text = "WAIT"
+        case .green:
+            yellowView.alpha = lightIsOff
+            greenView.alpha = lithtIsOn
+            currentLight = .red
+            messageLabel.textColor = .green
+            messageLabel.text = "GO"
+        }
+    }
+}
